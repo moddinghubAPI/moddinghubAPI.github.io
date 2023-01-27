c18e919db407c90d1fa66b5a6d673a70d5e13132
moddinghubAPI <123670432+moddinghubAPI@users.noreply.github.com>
2023-01-27 08:37:05 -0600

    Nobile

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-
+   function OnStart(){ 
+      if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+        document.getElementById("alltext1").style.visibility="hidden";
+        document.getElementById("alltext2").style.visibility="hidden";
+        document.getElementById("alltext3").style.visibility="hidden";
+        document.getElementById("button1").style.visibility="hidden";
+	      document.getElementByClass("button").style.visibility="hidden";
+	      document.getElementById("NoBile").style.visibility="visable";
+	      console.log("This site does not support mobile");
+      }else{
+        console.log("mobile check:off");
+      }
+    }
